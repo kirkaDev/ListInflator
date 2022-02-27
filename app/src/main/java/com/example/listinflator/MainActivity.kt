13a0f657a080f023f9a828b22ec9aaae9592ad16
Kirill <kirkadev@gmail.com>
2022-02-27 19:39:30 +0500

    Main controller added

--- app/src/main/java/com/example/listinflator/MainActivity.kt
+++ app/src/main/java/com/example/listinflator/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.listinflator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import com.example.listinflator.ui.settings.SettingsController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var router: Router
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        router =
+            Conductor.attachRouter(this, findViewById(R.id.activityContainer), savedInstanceState)
+
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(SettingsController()))
+        }
     }
 }
---
+++ app/src/main/java/com/example/listinflator/ui/main/MainController.kt
@@ -0,0 +1,16 @@
+package com.example.listinflator.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainController: MvpController() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup,
+        savedViewState: Bundle?
+    ): View {
+        TODO("Not yet implemented")
+    }
+}
