4991fd91a45109ca69b1544da876c7265858efea
Kirill <kirkadev@gmail.com>
2022-03-02 22:10:15 +0500

    Setting model refactored

--- app/src/main/java/com/example/listinflator/data/model/Setting.kt
+++ app/src/main/java/com/example/listinflator/data/model/Setting.kt
@@ -5,5 +5,10 @@ import androidx.annotation.DrawableRes
 data class Setting(
     @DrawableRes val imgResource: Int,
     val name: String,
-    val state: String
-)
+    val state: String,
+    val settingType: SettingTypes
+){
+    enum class SettingTypes{
+        AIRPLANE_MODE, MOBILE_DATA, WI_FI, BLUETOOTH, AIR_DROP, HOTSPOT
+    }
+}
